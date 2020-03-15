2aacb1b8d2718efddaeae61364194fe2792af788
holymoly <holymoly@Maurices-MacBook-Pro.local>
2020-03-15 17:53:13 +0100

    added rainbow to wtf message

--- mqttRequest.ino
+++ mqttRequest.ino
@@ -28,8 +28,9 @@ void mqttReqWtf(const JsonDocument& message){
   // Just adjust  doc["device"] = mqttNodeName 
   //              doc["uid"]  = message["uid"];
 
-  const size_t capacity = JSON_ARRAY_SIZE(4) + JSON_OBJECT_SIZE(2) + 2*JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6);
+  const size_t capacity = JSON_ARRAY_SIZE(6) + 2*JSON_OBJECT_SIZE(2) + 3*JSON_OBJECT_SIZE(3) + 2*JSON_OBJECT_SIZE(6);
   DynamicJsonDocument doc(capacity);
+
   char jsonString[capacity];
 
   char topic[300];
@@ -37,42 +38,47 @@ void mqttReqWtf(const JsonDocument& message){
   doc["type"] = "resp";
   doc["description"] = "You can use the message inside the wtf array for testing";
   doc["device"] = mqttNodeName;  
+  doc["leds"] = NUMPIXELS; 
   doc["uid"] = message["uid"];
 
   JsonArray wtf = doc.createNestedArray("wtf");
-
+  
   JsonObject wtf_0 = wtf.createNestedObject();
   wtf_0["type"] = "cmd";
   wtf_0["cmd"] = "setProgram";
   wtf_0["value"] = "BALL";
-
+  
   JsonObject wtf_1 = wtf.createNestedObject();
-  wtf_0["type"] = "cmd";
-  wtf_0["cmd"] = "setProgram";
-  wtf_0["value"] = "BLINK";
-
-  JsonObject wtf_2 = wtf.createNestedObject();
   wtf_1["type"] = "cmd";
   wtf_1["cmd"] = "setProgram";
-  wtf_1["value"] = "MANUAL";
-  wtf_1["red"] = "125";
-  wtf_1["green"] = "125";
-  wtf_1["blue"] = "125";
-
-  JsonObject wtf_3 = wtf.createNestedObject();
+  wtf_1["value"] = "BLINK";
+  
+  JsonObject wtf_2 = wtf.createNestedObject();
   wtf_2["type"] = "cmd";
-  wtf_2["cmd"] = "wifiReset";
-
-  JsonObject wtf_4 = wtf.createNestedObject();
+  wtf_2["cmd"] = "setProgram";
+  wtf_2["value"] = "MANUAL";
+  wtf_2["red"] = "125";
+  wtf_2["green"] = "125";
+  wtf_2["blue"] = "125";
+  
+  JsonObject wtf_3 = wtf.createNestedObject();
   wtf_3["type"] = "cmd";
-  wtf_3["cmd"] = "debug";
-  wtf_3["value"] = "on";
+  wtf_3["cmd"] = "setProgram";
+  wtf_3["value"] = "RAINBOW";
+  
+  JsonObject wtf_4 = wtf.createNestedObject();
+  wtf_4["type"] = "cmd";
+  wtf_4["cmd"] = "wifiReset";
+  
+  JsonObject wtf_5 = wtf.createNestedObject();
+  wtf_5["type"] = "cmd";
+  wtf_5["cmd"] = "debug";
 
   serializeJson(doc, jsonString);
 
   //build topic
   //todo build topic string ones and not on every debug message
-  if(snprintf (topic, 299, "%s/space/%s/%s/wtf/", mqttDeviceType, mqttRoomName, mqttNodeName) > 0){
+  if(snprintf (topic, 300, "%s/space/%s/%s/wtf/", mqttDeviceType, mqttRoomName, mqttNodeName) > 0){
     Serial.print("Send wtf message to: ");
     Serial.println(topic);
     mqttClient.publish(topic, 1, true, jsonString);
